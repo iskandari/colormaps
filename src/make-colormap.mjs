620e89761f3e47cd7f546e5b620bd1d26ddf2621
iskandari <sasha.tedeschi@gmail.com>
2025-01-28 07:51:42 +0200

    changed dark mode start color

--- src/make-colormap.mjs
+++ src/make-colormap.mjs
@@ -29,7 +29,7 @@ const makeColormap = (name, options) => {
   let start, middle, end
 
   if (mode === 'dark') {
-    start = chroma('#1b1e23').brighten(0.3)
+    start = chroma('#1b1c5f').brighten(0)
     middle = chroma('#808080').brighten(0.6)
     end = chroma('#ebebec')
   }
